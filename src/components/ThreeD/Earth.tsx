04ff1a9478cdf14345a54db242b3935d53c87e5e
AlphaCupcake10 <alphacupcake@outlook.com>
2024-04-23 22:02:34 +0530

    Animation Setup

--- src/components/ThreeD/Earth.tsx
+++ src/components/ThreeD/Earth.tsx
@@ -1,11 +1,85 @@
 import { useGLTF } from '@react-three/drei'
+import { useFrame, RootState } from '@react-three/fiber';
+import { useLenis } from '@studio-freight/react-lenis';
+import { useRef, useEffect, RefObject } from 'react';
+import { AnimationMixer, Object3D } from 'three';
 
-function Earth() {
+function Earth(props:{scrollRef?:RefObject<HTMLDivElement>}) {
     const gltf = useGLTF('Earth.glb');
+    const globalMixer = useRef<AnimationMixer>();
+    const cameraMixer = useRef<AnimationMixer>();
+    const AnimCamera = useRef<Object3D>();
+    const scrollDuration = 540/30;
+    const scrollPercent = useRef(0);
 
-    console.log(gltf)
+    const CameraClips:string[] = []
+
+    function getScrollPercent() {
+        var scrollTop = window.pageYOffset !== undefined ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+        var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+        var bodyHeight = Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+        );
+        var scrollPercent = (scrollTop / (bodyHeight - windowHeight));
+        return scrollPercent;
+    }
+
+    useLenis(() =>
+    {
+        if(!props?.scrollRef?.current)return;        
+        scrollPercent.current = getScrollPercent();
+    })
+
+    useFrame((state, delta) => {
+        if (!state) return;//IDK WHY TODO CHANGE
+        if (!globalMixer.current) CreateGlobalMixer();
+        if (!cameraMixer.current) CreateCameraMixer();
+        globalMixer.current?.update(delta);
+
+        cameraMixer.current?.setTime(scrollPercent.current*scrollDuration*.99);
+
+        SetCameraPosition(state);
+    })
+
+    function CreateGlobalMixer() {
+        if (gltf.animations.length) {
+            globalMixer.current = new AnimationMixer(gltf.scene);
+            gltf.animations.forEach(clip => {
+                if (CameraClips.includes(clip.name)) return;
+                const action = globalMixer.current?.clipAction(clip)
+                action?.play();
+            });
+        }
+    }
+    function CreateCameraMixer() {
+        if (gltf.animations.length) {
+            cameraMixer.current = new AnimationMixer(gltf.scene);
+            gltf.animations.forEach(clip => {
+                if (!CameraClips.includes(clip.name)) return;
+                const action = cameraMixer.current?.clipAction(clip)
+                action?.play();
+            });
+        }
+
+    }
+
+    useEffect(() => {
+        AnimCamera.current = gltf.scene.getObjectByName("CamPivot")?.getObjectByName("Camera");
+    }, [gltf])
+
+    function SetCameraPosition(state: RootState) {
+        if (!AnimCamera.current) return;
+        AnimCamera.current.getWorldPosition(state.camera.position);
+        AnimCamera.current.getWorldQuaternion(state.camera.quaternion);
+        state.camera.updateMatrixWorld();
+    }
+
     return (
-        <primitive object={gltf.scene} />
+        <>
+            <primitive object={gltf.scene} />
+        </>
     )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber"
 import Earth from "../components/ThreeD/Earth";
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
@@ -8,14 +7,13 @@ function Home()
   return (
     <>
       <main className="h-screen relative flex div-center">
-        <div className="div-center pointer-events-none z-10">
-          <h1 className="text-[1vw] font-bold tracking-[2vw] leading-[4vw] uppercase">Astroterra</h1>
+        <div className="z-10 p-10 overflow-hidden group cursor-pointer">
+          <h1 className="text-[1vw] absolute font-bold tracking-[2vw] leading-[4vw] uppercase opacity-0 group-hover:opacity-100 duration-300 translate-y-full group-hover:translate-y-0">EXPLORE</h1>
+          <h1 className="text-[1vw] font-bold tracking-[2vw] leading-[4vw] uppercase group-hover:opacity-0 duration-300 translate-y-0 group-hover:-translate-y-full">Astroterra</h1>
         </div>
         <div className="absolute w-full h-full top-0 left-0">
-          <Canvas className="" camera={{fov:30}}>
-              <directionalLight color={"#7ea9ed"} intensity={5} position={[1,0,-2]} />
+          <Canvas className="" camera={{fov:40}}>
               <Earth />
-              {/* <OrbitControls/> */}
               <EffectComposer>
                   <Bloom mipmapBlur intensity={1.2} />
               </EffectComposer>
